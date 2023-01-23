30fbf0ff9fe09bb1bde1a8a05e7c5678f641029b
Paw1915 <piotrandrzejwalczak@gmail.com>
2023-01-23 17:36:33 +0100

    Add shopping_list,py

---
+++ shopping_list.py
@@ -0,0 +1,16 @@
+shopping_list = {
+    "piekarnia": ["chleb", "pączek", "bułki"],
+    "warzywniak": ["marchew", "seler", "rukola"]
+}
+shopping_list["piekarnia"] = [f.capitalize() for f in shopping_list["piekarnia"]]
+shopping_list["warzywniak"] = [f.capitalize() for f in shopping_list["warzywniak"]]
+
+count = 0
+for key, value in shopping_list.items():
+    if isinstance(value, list):
+         count += len(value)
+
+print("Lista zakupów:")
+for key in shopping_list:
+    print(f"Idę do ", key.capitalize(), ", kupuję tu następujące rzeczy:", shopping_list[key],)
+print("W sumie kupuję", count, "produktów.")
